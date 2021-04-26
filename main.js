8afd5eed430233a5ca77cdd29b245f99b7f52e95
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-04-26 18:31:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//declaracion---funcion descriptiva
+// function nombre(...p1){  //parametro
+//  console.log(p1);
+// return `ya se ejecuto el dato`;
+// }
+ 
+ 
+// let resultado=nombre("andres");argumento
+// nombre("andres");
+// //funciones por expresión 
+
+// let nombre =function(p1){
+// 
+    // return `funcion dato ${p1}`
+// }
+// console.log(nombre("Andres"));
+
+nombre="andres"
+function saludar(){
+    console.log(nombre);
+}
+saludar();
